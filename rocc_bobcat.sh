7aa0322b6fdc81ac2f0d0a7fbcbccc0c87b4427d
Ckristian Duran <ckdur.iso@gmail.com>
2023-10-26 17:45:47 +0900

    Just saving progress of the load interface

--- rocc_bobcat.sh
+++ rocc_bobcat.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 commands=(
-#  "make -C sims/verilator run-binary-debug CONFIG=RocketVecRoccExampleConfig BINARY=$PWD/tests/rvv/bringup_tests/ms1_vmv_vi.elf EXTRA_SIM_FLAGS=--verbose"
-  "make -C sims/verilator run-binary-debug-hex CONFIG=RocketVecRoccExampleConfig BINARY=$PWD/tests/rvv/bringup_tests/ms1_vmv_vi.elf EXTRA_SIM_FLAGS=--verbose"
-  "make -C sims/verilator run-binary-debug-hex CONFIG=RocketVecRoccExampleConfig BINARY=$PWD/tests/rvv/bringup_tests/ms2_vse64.elf EXTRA_SIM_FLAGS=--verbose"
-  "make -C sims/verilator run-binary-debug-hex CONFIG=RocketVecRoccExampleConfig BINARY=$PWD/tests/rvv/isg/riscv_vector_arithmetic_smoke_test.elf EXTRA_SIM_FLAGS=--verbose"
+#  "make -C sims/verilator run-binary-debug-hex CONFIG=RocketVecRoccExampleConfig BINARY=$PWD/tests/rvv/bringup_tests/ms1_vmv_vi.elf EXTRA_SIM_FLAGS=--verbose"
+#  "make -C sims/verilator run-binary-debug-hex CONFIG=RocketVecRoccExampleConfig BINARY=$PWD/tests/rvv/bringup_tests/ms2_vse64.elf EXTRA_SIM_FLAGS=--verbose"
+#  "make -C sims/verilator run-binary-debug-hex CONFIG=RocketVecRoccExampleConfig BINARY=$PWD/tests/rvv/isg/riscv_vector_arithmetic_smoke_test.elf EXTRA_SIM_FLAGS=--verbose"
+#  "make -C sims/verilator run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=$PWD/tests/rvv/kernels/axpy/axpy-vector.elf SIM_FLAGS=+cosim"
+  "make -C sims/verilator run-binary-debug-hex CONFIG=RocketVecRoccExampleConfig BINARY=$PWD/tests/rvv/kernels/axpy/axpy-vector.elf EXTRA_SIM_FLAGS=--verbose"
 )
 
 for cmd in "${commands[@]}"
