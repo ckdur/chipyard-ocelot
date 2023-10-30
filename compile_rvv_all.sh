a653aff25c8271c1ea4922d87afa42e49d13c86e
Ckristian Duran <ckdur.iso@gmail.com>
2023-10-30 10:03:27 +0900

    Using RVV1.0 instead of EPI RVV0.7

--- compile_rvv_all.sh
+++ compile_rvv_all.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 
-export LLVM=$PWD/llvm-EPI-0.7-development-toolchain-cross
+#export LLVM=$PWD/llvm-EPI-0.7-development-toolchain-cross
+export LLVM=/opt/clang
+export LLVM_DOWN=llvm-EPI-0.7-development-toolchain-cross-latest.tar.bz2
 export PATH=$PATH:$LLVM/bin
 
 # Get the toolchain if necessary
 if ! test -f ${LLVM}/bin/clang; then
-  if ! test -f llvm-EPI-0.7-development-toolchain-cross-latest.tar.bz2; then
-    wget https://ssh.hca.bsc.es/epi/ftp/llvm-EPI-0.7-development-toolchain-cross-latest.tar.bz2
+  if ! test -f ${LLVM_DOWN}; then
+    wget https://ssh.hca.bsc.es/epi/ftp/${LLVM_DOWN}
   fi
-  tar -xvf llvm-EPI-0.7-development-toolchain-cross-latest.tar.bz2
+  tar -xvf ${LLVM_DOWN}
 fi
 
 # Compile the riscv-vectorized-benchmark, just for health checks
@@ -27,23 +29,25 @@ LDFLAGS="-march=rv64g -mabi=lp64d -static -nostdlib -nostartfiles -O2 -lm -lc -T
 #${PWD}/tests/rvv/bringup_tests/bobcat_linker.ld
 #${RVTESTS}/benchmarks/common/test.ld
 
-# Compile the axpy
-AXPY=${RVSUITE}/_axpy
+# Compile some commons (Ej: the CRT and the syscalls)
 riscv64-unknown-elf-gcc ${CFLAGS} -fno-tree-loop-distribute-patterns -c -o ${RVTESTS}/benchmarks/common/crt.o ${RVTESTS}/benchmarks/common/crt.S
 riscv64-unknown-elf-gcc ${CFLAGS} -fno-tree-loop-distribute-patterns -c -o ${RVTESTS}/benchmarks/common/syscalls.o ${RVTESTS}/benchmarks/common/syscalls.c
 riscv64-unknown-elf-gcc ${CFLAGS} -fno-tree-loop-distribute-patterns -c -o ${PWD}/tests/rvv/sbrk.o ${PWD}/tests/rvv/sbrk.c
 riscv64-unknown-elf-gcc ${CFLAGS} -fno-tree-loop-distribute-patterns -c -o ${PWD}/tests/rvv/gettimeofday.o ${PWD}/tests/rvv/gettimeofday.c
+
+# Compile the test suite axpy
+AXPY=${RVSUITE}/_axpy
 ${LLVM}/bin/clang --target=riscv64-unknown-elf ${CFLAGS} -c -o ${AXPY}/src/utils.o ${AXPY}/src/utils.c
-${LLVM}/bin/clang --target=riscv64-unknown-elf ${CFLAGS} -mepi -DUSE_RISCV_VECTOR -fno-vectorize -c -o ${AXPY}/src/axpy.o ${AXPY}/src/axpy.c
+${LLVM}/bin/clang --target=riscv64-unknown-elf ${CFLAGS} -DUSE_RISCV_VECTOR -fno-vectorize -c -o ${AXPY}/src/axpy.o ${AXPY}/src/axpy.c
 ${LLVM}/bin/clang --target=riscv64-unknown-elf ${CFLAGS} -O2 -c -o ${AXPY}/src/main.o ${AXPY}/src/main.c
 riscv64-unknown-elf-gcc ${CFLAGS} -o ${AXPY}/bin/axpy_vector.exe ${RVTESTS}/benchmarks/common/crt.o ${RVTESTS}/benchmarks/common/syscalls.o ${AXPY}/src/*.o ${PWD}/tests/rvv/*.o ${LDFLAGS}
 rm ${AXPY}/src/*.o
 ${LLVM}/bin/llvm-objdump  --mattr=+m,+f,+d,+a,+c,+experimental-v -ds  ${AXPY}/bin/axpy_vector.exe > ${AXPY}/bin/axpy_vector.dump
 
-# Compile the "other" axpy
+# Compile the "kernel" axpy
 KERNEL_AXPY=$PWD/tests/rvv/kernels/_axpy
 ${LLVM}/bin/clang --target=riscv64-unknown-elf ${CFLAGS} -c -o ${KERNEL_AXPY}/src/utils.o ${KERNEL_AXPY}/src/utils.c
-${LLVM}/bin/clang --target=riscv64-unknown-elf ${CFLAGS} -mepi -DUSE_RISCV_VECTOR -fno-vectorize -c -o ${KERNEL_AXPY}/src/axpy.o ${KERNEL_AXPY}/src/axpy.c
+${LLVM}/bin/clang --target=riscv64-unknown-elf ${CFLAGS} -DUSE_RISCV_VECTOR -fno-vectorize -c -o ${KERNEL_AXPY}/src/axpy.o ${KERNEL_AXPY}/src/axpy.c
 ${LLVM}/bin/clang --target=riscv64-unknown-elf ${CFLAGS} -O2 -c -o ${KERNEL_AXPY}/src/main.o ${KERNEL_AXPY}/src/main.c
 riscv64-unknown-elf-gcc ${CFLAGS} -o ${KERNEL_AXPY}/bin/axpy_vector.exe ${RVTESTS}/benchmarks/common/crt.o ${RVTESTS}/benchmarks/common/syscalls.o ${KERNEL_AXPY}/src/*.o ${PWD}/tests/rvv/*.o ${LDFLAGS}
 rm ${KERNEL_AXPY}/src/*.o
