7a4be26f1bbabfe2cb09b2f4fa077a7ccc032918
Ckristian Duran <ckdur.iso@gmail.com>
2023-12-01 15:20:58 +0900

    Miscellaneous configuration changes

--- generators/chipyard/src/main/scala/config/BobcatConfigs.scala
+++ generators/chipyard/src/main/scala/config/BobcatConfigs.scala
@@ -2,6 +2,7 @@ package chipyard
 
 import org.chipsalliance.cde.config.{Config}
 import freechips.rocketchip.devices.tilelink.{BootROMLocated}
+import freechips.rocketchip.subsystem._
 
 // ---------------------
 // Bobcat Configs
@@ -14,15 +15,22 @@ class LittleBobcatConfig extends Config(
     new boom.common.WithNLittleBooms(1) ++                          // small boom config
     new chipyard.config.AbstractConfig)
 
+class ChangeExtMemBase(address: BigInt) extends Config((site, here, up) => {
+  case ExtMem => up(ExtMem, site).map(x => x.copy(master = x.master.copy(base = address)))
+})
+
 class MicroBobcatConfig extends Config(
   new boom.common.WithVector(1) ++
     new boom.common.WithBoomDebugHarness ++ // Enable debug harness
     new WithCustomBootROM ++ // Use custom BootROM to enable COSIM
     new chipyard.config.WithTLSerialLocation(freechips.rocketchip.subsystem.FBUS, freechips.rocketchip.subsystem.SBUS) ++
-    new chipyard.example.WithMySRAMKey ++
-    //new freechips.rocketchip.subsystem.WithIncoherentBusTopology ++
+    // new freechips.rocketchip.subsystem.WithIncoherentBusTopology ++
     //new freechips.rocketchip.subsystem.WithNBanks(0) ++ // remove L2$
-    new freechips.rocketchip.subsystem.WithNoMemPort ++ // remove backing memory
+    new testchipip.WithBackingScratchpad(base = 0x80000000L, mask = (128 << 10) - 1) ++
+    // new freechips.rocketchip.subsystem.WithNoMemPort ++ // remove backing memory
+    new freechips.rocketchip.subsystem.WithDefaultSlavePort ++
+    new freechips.rocketchip.subsystem.WithoutTLMonitors ++
+    new ChangeExtMemBase(0x90000000L) ++
     new chipyard.config.WithBroadcastManager ++ // no l2
     new boom.common.WithNLittleBooms(1) ++ // small boom config
     new chipyard.config.AbstractConfig)
